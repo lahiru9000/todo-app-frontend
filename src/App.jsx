4e338c2d626d9d76ea3b6c164b3df3e34c204653
Lahiru Sandaruwan <lahirus250@gmail.com>
2025-01-04 21:18:58 +0530

    initial backend API call with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/todos")
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.header}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
