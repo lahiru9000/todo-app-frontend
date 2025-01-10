6f7baff86bed4676d337a268afe1be81564660f9
Lahiru Sandaruwan <lahirus250@gmail.com>
2025-01-10 21:33:55 +0530

    integrate UI with CRUD APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Header from "./Header/Header";
+import Body from "./Body/Body";
+import Footer from "./Footer/Footer";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:9000/todos")
-      .then((response) => setTodos(response.data))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.header}</li>
-      ))}
-    </ul>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
 
